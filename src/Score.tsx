fbe07760e7c4f488521fad408501b99fc692a4d0
sjcco <73748496+sjcco@users.noreply.github.com>
2023-04-14 02:29:59 -0600

    Update display + Add pause button

--- src/Score.tsx
+++ src/Score.tsx
@@ -1,23 +1,31 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import useTimer from "./hooks/useTimer";
 import './Score.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
+import { states } from "./interfaces";
 
 interface ScoreProps {
   pairsFound: number;
   gameRunning: boolean;
+  openControl: (state: states) => void;
+  timer: {hours: string, minutes: string, seconds: string, totalSeconds: number}
 }
 
-const Score: FC<ScoreProps> = ({gameRunning, pairsFound}) => {
-  const timer = useTimer(gameRunning)
+const Score: FC<ScoreProps> = ({gameRunning, pairsFound, openControl, timer}) => {
+  
   return <section className="top-bar">
     <nav>
-      <div className="score">
-        score: <span>{pairsFound}</span>
+      <div>
+        <button onClick={()=> openControl('paused')}>Pause <FontAwesomeIcon icon={faPause} /></button>
+      </div>
+      <div className="pairs-found">
+        <p>Pairs found: <span>{pairsFound}</span></p>
       </div>
       <div className="timer">
-        <span>{timer.hours}</span>:
-        <span>{timer.minutes}</span>:
-        <span>{timer.seconds}</span>
+        <p>{timer.hours}:</p>
+        <p>{timer.minutes}:</p>
+        <p>{timer.seconds}</p>
       </div>
     </nav>
   </section>
